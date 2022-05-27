09413c9d26317db958fff655dcf41dcf63a90b72
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2022-05-27 10:54:33 -0300

    Implementando o CategoryValidator test

--- domain/src/test/java/com/fullcycle/admin/catalogo/domain/category/CategoryTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/category/CategoryTest.java
@@ -1,17 +1,20 @@
 package com.fullcycle.admin.catalogo.domain.category;
 
+import com.fullcycle.admin.catalogo.domain.exceptions.DomainException;
+import com.fullcycle.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class CategoryTest {
+public class CategoryTest {
+
     @Test
-    void givenAValidParams_whenCallNewCategory_thenInstantiateACategory() {
+    public void givenAValidParams_whenCallNewCategory_thenInstantiateACategory() {
         final var expectedName = "Filmes";
         final var expectedDescription = "A categoria mais assistida";
         final var expectedIsActive = true;
 
         final var actualCategory =
-                    Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+                Category.newCategory(expectedName, expectedDescription, expectedIsActive);
 
         Assertions.assertNotNull(actualCategory);
         Assertions.assertNotNull(actualCategory.getId());
@@ -24,20 +27,20 @@ class CategoryTest {
     }
 
     @Test
-    void givenAnInValidNullName_whenCallNewCategoryAndValidate_thenShouldRecievError() {
+    public void givenAnInvalidNullName_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
         final String expectedName = null;
-        final var expectedDescription = "A categoria mais assistida";
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = "'name' should not be null";
+        final var expectedDescription = "A categoria mais assistida";
         final var expectedIsActive = true;
 
         final var actualCategory =
                 Category.newCategory(expectedName, expectedDescription, expectedIsActive);
 
         final var actualException =
-                Assertions.assertThrows(DomainException.class, ()-> actualCategory.validate());
+                Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
 
-        Assertions.assertEquals(expectedErrorCount,actualException.getErrors().size());
-        Assertions.assertEquals(expectedErrorMessage,actualException.getErrors().get(0).getMessage());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
     }
 }
